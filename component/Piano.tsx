0c288e15f3873e48ecfebb95a9858117825f3b70
yiqing.liang <yiqing.liang@hotmail.com>
2025-03-10 21:46:43 +0200

    align key text under bottom

--- component/Piano.tsx
+++ component/Piano.tsx
@@ -1,93 +1,86 @@
 import { View, TouchableOpacity, StyleSheet, Text } from "react-native";
 
 const Piano: React.FC = () => {
- 
-
   return (
     <View style={styles.piano}>
-            <TouchableOpacity key={`white-do`} style={[styles.key, styles.whiteKey]}>
+      <TouchableOpacity key={`white-do`} style={[styles.key, styles.whiteKey]}>
         <Text style={styles.keyLabel}>do</Text>
       </TouchableOpacity>
       <TouchableOpacity
         key={`black-do-half`}
         style={[styles.key, styles.blackKey, { left: 25 }]}
       />
-      <TouchableOpacity
-        key={`white-re`}
-        style={[styles.key, styles.whiteKey]}
-      />
+      <TouchableOpacity key={`white-re`} style={[styles.key, styles.whiteKey]}>
+        <Text style={styles.keyLabel}>re</Text>
+      </TouchableOpacity>
       <TouchableOpacity
         key={`black-re-half`}
-        style={[styles.key, styles.blackKey, { left: 35 +25 }]}
+        style={[styles.key, styles.blackKey, { left: 35 + 25 }]}
       />
-
+      <TouchableOpacity key={`white-mi`} style={[styles.key, styles.whiteKey]}>
+        <Text style={styles.keyLabel}>mi</Text>
+      </TouchableOpacity>
+      <TouchableOpacity key={`white-fa`} style={[styles.key, styles.whiteKey]}>
+        <Text style={styles.keyLabel}>fa</Text>
+      </TouchableOpacity>
       <TouchableOpacity
-        key={`white-mi`}
-        style={[styles.key, styles.whiteKey]}
-      />
-       <TouchableOpacity
-        key={`white-fa`}
-        style={[styles.key, styles.whiteKey]}
-      />
-       <TouchableOpacity
         key={`black-fa-half`}
-        style={[styles.key, styles.blackKey, { left: 3*35 +25 }]}
+        style={[styles.key, styles.blackKey, { left: 3 * 35 + 25 }]}
       />
+      <TouchableOpacity key={`white-so`} style={[styles.key, styles.whiteKey]}>
+        <Text style={styles.keyLabel}>so</Text>
+      </TouchableOpacity>
       <TouchableOpacity
-        key={`white-so`}
-        style={[styles.key, styles.whiteKey]}
-      />
-       <TouchableOpacity
         key={`black-so-half`}
-        style={[styles.key, styles.blackKey, { left: 4*35 +25 }]}
+        style={[styles.key, styles.blackKey, { left: 4 * 35 + 25 }]}
       />
-        <TouchableOpacity
-        key={`white-la`}
-        style={[styles.key, styles.whiteKey]}
-      />
-         <TouchableOpacity
+      <TouchableOpacity key={`white-la`} style={[styles.key, styles.whiteKey]}>
+        <Text style={styles.keyLabel}>la</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
         key={`black-la-half`}
-        style={[styles.key, styles.blackKey, { left: 5*35 +25 }]}
+        style={[styles.key, styles.blackKey, { left: 5 * 35 + 25 }]}
       />
-       <TouchableOpacity
-        key={`white-si`}
-        style={[styles.key, styles.whiteKey]}
-      />
+      <TouchableOpacity key={`white-si`} style={[styles.key, styles.whiteKey]}>
+        <Text style={styles.keyLabel}>si</Text>
+      </TouchableOpacity>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-    piano: {
-      flexDirection: "row",
-      marginTop: 20,
-      borderWidth: 1,
-      borderColor: "#ccc",
-      borderRadius: 5,
-      overflow: "hidden",
-      position: "relative",
-    },
-    key: {
-      height: 150,
-      margin: 1,
-      borderWidth: 1,
-      borderColor: "#ccc",
-    },
-    whiteKey: {
-      width: 35,
-      backgroundColor: "#fff",
-    },
-    blackKey: {
-      width: 30,
-      backgroundColor: "#000",
-      height: 100,
-      position: "absolute",
-      zIndex: 1,
-    },
-    keyLabel: {
-        color: "#000",
-        marginTop: 5,
-      },
-  });
-  
+  piano: {
+    flexDirection: "row",
+    marginTop: 20,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 5,
+    overflow: "hidden",
+    position: "relative",
+  },
+  key: {
+    height: 150,
+    margin: 1,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  whiteKey: {
+    width: 35,
+    backgroundColor: "#fff",
+  },
+  blackKey: {
+    width: 30,
+    backgroundColor: "#000",
+    height: 100,
+    position: "absolute",
+    zIndex: 1,
+  },
+  keyLabel: {
+    color: "#000",
+    marginBottom: 5,
+  },
+});
+
 export default Piano;
